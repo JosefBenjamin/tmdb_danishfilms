e16f6dd32ea87d8a8d7543c782701cd3b4274e60
73N37 <x0d4r4p@proton.me>
2025-09-16 12:06:40 +0200

    added more fields to entities

--- src/main/java/app/entities/Director.java
+++ src/main/java/app/entities/Director.java
@@ -1,25 +1,31 @@
 package app.entities;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Builder;
-import lombok.ToString;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.*;
 import jakarta.persistence.Entity;
 
+import java.util.HashSet;
+import java.util.Set;
 
 
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "directors")
 public class Director {
+    @Column(name = "director_name", nullable = false, length = 255)
     private String name;
+
+    @Column(name = "director_age", nullable = false)
     private int age;
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "director_id")
     private Long id;
+
+    @ManyToMany(mappedBy = "directors", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Actor> actors = new HashSet<>();
 
     public void setId(Long id) {
         this.id = id;
@@ -29,3 +35,4 @@ public class Director {
         return id;
     }
 }
+
